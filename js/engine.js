0d32b4ea24090c9650b4f652e79d5c8e0b56651e
kkas <kentakikui@gmail.com>
2015-11-23 20:36:15 +0900

    feat: add how-to-play

--- js/engine.js
+++ js/engine.js
@@ -23,6 +23,7 @@ var Engine = (function(global) {
         win = global.window,
         canvas = doc.createElement('canvas'),
         ctx = canvas.getContext('2d'),
+        section = document.querySelector(".game-canvas"),
         lastTime,
         numRows = 6,
         numCols = 5,
@@ -31,7 +32,7 @@ var Engine = (function(global) {
 
     canvas.width = 505;
     canvas.height = 606;
-    doc.body.appendChild(canvas);
+    section.appendChild(canvas);
 
     /* This function serves as the kickoff point for the game loop itself
      * and handles properly calling the update and render methods.
